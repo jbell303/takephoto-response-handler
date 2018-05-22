6f067df417fd0327681f3ecb26d14c702712b108
James Bell <jbell303@gmail.com>
2018-05-22 13:09:36 -0700

    update handler code

--- index.js
+++ index.js
@@ -6,35 +6,55 @@
   James and Audrey Bell 2018
 */
 
-// import the necessary packages
-var http = require('http');
-var url = 'http://alexaraspicam.ngrok.io/photo'; // unique tunneling url to raspberry pi server
+'use strict';
 const Alexa = require('alexa-sdk');
-const APP_ID = 'amzn1.ask.skill.41fd0b69-d293-4ab6-9292-3496b2a89f1c'; // unique app id for alexa skill
+var http = require('http');
+const URL = 'http://alexaraspicam.ngrok.io/photo'; // unique tunneling url to raspberry pi server
 
-// AWS Lambda response handler
-exports.handler = (event, context, callback) => {
+const APP_ID = 'amzn1.ask.skill.41fd0b69-d293-4ab6-9292-3496b2a89f1c';
+
+const SKILL_NAME = 'Raspberry Pie';
+const HELP_MESSAGE = 'I can take a photo using your raspberry pie... Just tell me to take a picture';
+const HELP_REPROMPT = 'If you want me to take a photo of you, just say take a picture';
+const STOP_MESSAGE = 'Goodbye!';
+
+const handlers = {
+    'LaunchRequest': function () {
+        this.emit('AMAZON.HelpIntent');
+    },
+    'cameraControlIntent': function () {
+        const speechOutput = "taking a picture!";
+        this.response.speak(speechOutput);
+        this.emit(':responseReady');
+
+        // perform GET request to raspberry pi server
+        http.get(URL, function(res) {
+            console.log("Response: " + res.statusCode);
+        }).on('error', function(e) {
+            console.log("Error: " + e.message);
+        });
+        console.log('end request to ' + URL);
+    },
+    'AMAZON.HelpIntent': function () {
+        const speechOutput = HELP_MESSAGE;
+        const reprompt = HELP_REPROMPT;
+
+        this.response.speak(speechOutput).listen(reprompt);
+        this.emit(':responseReady');
+    },
+    'AMAZON.CancelIntent': function () {
+        this.response.speak(STOP_MESSAGE);
+        this.emit(':responseReady');
+    },
+    'AMAZON.StopIntent': function () {
+        this.response.speak(STOP_MESSAGE);
+        this.emit(':responseReady');
+    },
+};
+
+exports.handler = function (event, context, callback) {
     const alexa = Alexa.handler(event, context, callback);
-    alexa.appId = APP_ID;
-
-    // perform GET request to raspberry pi server
-    http.get(url, function(res) {
-        console.log("Response: " + res.statusCode);
-        callback(null, 'success msg');
-    }).on('error', function(e) {
-        console.log("Error: " + e.message);
-        callback(new Error('failure'));
-    });
-
-    console.log('end request to ' + url);
-
+    alexa.APP_ID = APP_ID;
     alexa.registerHandlers(handlers);
     alexa.execute();
 };
-
-// Alexa response handler
-const handlers = {
-        'cameraControlIntent' : function() {
-            this.emit(':tell', "Taking a picture!"); // Alexa should vocalize "Taking a picture!"
-        }
-    }
